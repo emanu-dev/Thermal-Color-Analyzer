11ad19c0cd3a48cbabfa2afe49a38d47e8a360d7
Emanuel <front@entrecliques.com.br>
2018-04-09 11:59:59 -0300

    Add initial v1 files

---
+++ Form1.Designer.cs
@@ -0,0 +1,295 @@
+﻿namespace ThermalColorAnalyser
+{
+    partial class frmPrincipal
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmPrincipal));
+            this.btnCapture = new System.Windows.Forms.Button();
+            this.pbPreview = new System.Windows.Forms.PictureBox();
+            this.tckTemperature = new System.Windows.Forms.TrackBar();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.numTop = new System.Windows.Forms.NumericUpDown();
+            this.numLeft = new System.Windows.Forms.NumericUpDown();
+            this.numBottom = new System.Windows.Forms.NumericUpDown();
+            this.numRight = new System.Windows.Forms.NumericUpDown();
+            this.MainLoop = new System.Windows.Forms.Timer(this.components);
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tckTemperature)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTop)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLeft)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBottom)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numRight)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // btnCapture
+            // 
+            this.btnCapture.Location = new System.Drawing.Point(159, 329);
+            this.btnCapture.Name = "btnCapture";
+            this.btnCapture.Size = new System.Drawing.Size(132, 34);
+            this.btnCapture.TabIndex = 5;
+            this.btnCapture.Text = "Iniciar";
+            this.btnCapture.UseVisualStyleBackColor = true;
+            this.btnCapture.Click += new System.EventHandler(this.btnCapture_Click);
+            // 
+            // pbPreview
+            // 
+            this.pbPreview.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.pbPreview.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.pbPreview.Location = new System.Drawing.Point(159, 44);
+            this.pbPreview.Name = "pbPreview";
+            this.pbPreview.Size = new System.Drawing.Size(132, 94);
+            this.pbPreview.TabIndex = 1;
+            this.pbPreview.TabStop = false;
+            // 
+            // tckTemperature
+            // 
+            this.tckTemperature.BackColor = System.Drawing.SystemColors.Window;
+            this.tckTemperature.Cursor = System.Windows.Forms.Cursors.No;
+            this.tckTemperature.Enabled = false;
+            this.tckTemperature.Location = new System.Drawing.Point(50, 37);
+            this.tckTemperature.Maximum = 180;
+            this.tckTemperature.Name = "tckTemperature";
+            this.tckTemperature.Orientation = System.Windows.Forms.Orientation.Vertical;
+            this.tckTemperature.Size = new System.Drawing.Size(45, 310);
+            this.tckTemperature.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(42, 18);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 17);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Quente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(54, 350);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(36, 17);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Frio";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(11, 59);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(55, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Esquerda:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(17, 35);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(49, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Superior:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(23, 107);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(40, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Direita:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(23, 83);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(42, 13);
+            this.label6.TabIndex = 4;
+            this.label6.Text = "Inferior:";
+            // 
+            // numTop
+            // 
+            this.numTop.Location = new System.Drawing.Point(72, 33);
+            this.numTop.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numTop.Name = "numTop";
+            this.numTop.Size = new System.Drawing.Size(41, 20);
+            this.numTop.TabIndex = 1;
+            this.numTop.Value = new decimal(new int[] {
+            110,
+            0,
+            0,
+            0});
+            // 
+            // numLeft
+            // 
+            this.numLeft.Location = new System.Drawing.Point(72, 57);
+            this.numLeft.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numLeft.Name = "numLeft";
+            this.numLeft.Size = new System.Drawing.Size(41, 20);
+            this.numLeft.TabIndex = 2;
+            this.numLeft.Value = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            // 
+            // numBottom
+            // 
+            this.numBottom.Location = new System.Drawing.Point(72, 81);
+            this.numBottom.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numBottom.Name = "numBottom";
+            this.numBottom.Size = new System.Drawing.Size(41, 20);
+            this.numBottom.TabIndex = 3;
+            this.numBottom.Value = new decimal(new int[] {
+            150,
+            0,
+            0,
+            0});
+            // 
+            // numRight
+            // 
+            this.numRight.Location = new System.Drawing.Point(72, 105);
+            this.numRight.Maximum = new decimal(new int[] {
+            500,
+            0,
+            0,
+            0});
+            this.numRight.Name = "numRight";
+            this.numRight.Size = new System.Drawing.Size(41, 20);
+            this.numRight.TabIndex = 4;
+            this.numRight.Value = new decimal(new int[] {
+            90,
+            0,
+            0,
+            0});
+            // 
+            // MainLoop
+            // 
+            this.MainLoop.Interval = 50;
+            this.MainLoop.Tick += new System.EventHandler(this.MainLoop_Tick);
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label4);
+            this.groupBox1.Controls.Add(this.numRight);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.numBottom);
+            this.groupBox1.Controls.Add(this.label5);
+            this.groupBox1.Controls.Add(this.numLeft);
+            this.groupBox1.Controls.Add(this.label6);
+            this.groupBox1.Controls.Add(this.numTop);
+            this.groupBox1.Location = new System.Drawing.Point(159, 153);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(132, 149);
+            this.groupBox1.TabIndex = 6;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Margens";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(156, 21);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(44, 13);
+            this.label7.TabIndex = 4;
+            this.label7.Text = "Captura";
+            // 
+            // frmPrincipal
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(303, 378);
+            this.Controls.Add(this.groupBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.tckTemperature);
+            this.Controls.Add(this.pbPreview);
+            this.Controls.Add(this.btnCapture);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "frmPrincipal";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.Manual;
+            this.Text = "Thermal Color Analyser";
+            this.Load += new System.EventHandler(this.frmPrincipal_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pbPreview)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.tckTemperature)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numTop)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numLeft)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numBottom)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numRight)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnCapture;
+        private System.Windows.Forms.PictureBox pbPreview;
+        private System.Windows.Forms.TrackBar tckTemperature;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.NumericUpDown numTop;
+        private System.Windows.Forms.NumericUpDown numLeft;
+        private System.Windows.Forms.NumericUpDown numBottom;
+        private System.Windows.Forms.NumericUpDown numRight;
+        private System.Windows.Forms.Timer MainLoop;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,125 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using ScreenShotDemo;
+using LockBitsTest;
+
+namespace ThermalColorAnalyser
+{
+    public partial class frmPrincipal : Form
+    {
+        #region Dll Imports
+
+        [DllImport("User32.dll")]
+        private static extern IntPtr GetForegroundWindow();
+        ScreenCapture sc = new ScreenCapture();
+
+        #endregion
+
+        #region CaptureWindow Variables
+
+        IntPtr activeWindowHandle, oldWindowHandle;
+        Image image;
+        Bitmap bitmap;
+        LockBitmap lockBitmap;
+
+        #endregion
+
+        #region CalculateHUE Method Variables
+
+        Color currentColor;
+        double totalHUE = 0f;
+        double mediaHUE = 0f;
+        int totalPixels = 1;
+        
+        #endregion
+
+        #region Form Load and Initialize Methods
+
+        public frmPrincipal()
+        {
+            InitializeComponent();
+        }
+
+        private void frmPrincipal_Load(object sender, EventArgs e)
+        {
+            //Nothing to do here...
+        }
+
+        #endregion
+
+        #region Main Methods
+
+        private void CaptureWindow()
+        {
+            int top    = (int)numTop.Value;
+            int left   = (int)numLeft.Value;
+            int right  = (int)numRight.Value;
+            int bottom = (int)numBottom.Value;
+
+            activeWindowHandle = GetForegroundWindow();
+
+            image = sc.CaptureWindow(activeWindowHandle, top, left, right, bottom);
+
+            pbPreview.Image = image;
+            pbPreview.SizeMode = PictureBoxSizeMode.StretchImage;
+
+            bitmap = new Bitmap(image);
+            lockBitmap = new LockBitmap(bitmap);
+        }
+
+        private void CalculateHUE(LockBitmap lb)
+        {
+            totalHUE = 0f;
+
+            lb.LockBits();
+
+            totalPixels = (lb.Width * lb.Height);
+
+            for (int y = 0; y < lb.Height; y++)
+            {
+                for (int x = 0; x < lb.Width; x++)
+                {
+                    currentColor = lb.GetPixel(x, y);
+                    totalHUE += currentColor.GetHue();
+                }
+            }
+
+            mediaHUE = totalHUE / totalPixels;
+
+            lb.UnlockBits();
+
+            tckTemperature.Value = Math.Abs((int)mediaHUE - 180);
+        }
+
+        #endregion
+
+        #region Main Events
+
+        private void btnCapture_Click(object sender, EventArgs e)
+        {
+            oldWindowHandle = GetForegroundWindow();
+            
+            btnCapture.Text = (!MainLoop.Enabled) ? "Parar" : "Iniciar";
+
+            MainLoop.Enabled = !MainLoop.Enabled;
+        }
+
+        private void MainLoop_Tick(object sender, EventArgs e)
+        {
+            
+            CaptureWindow();
+            CalculateHUE(lockBitmap);
+        }
+
+        #endregion
+
+    }
+}
